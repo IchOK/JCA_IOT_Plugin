292b1d37e8a13ae89b65c86b698ac89518279691
Jochen Cabrera <jochen.matz@googlemail.com>
2019-11-03 01:21:53 +0100

    Create JCA_IOT.h

---
+++ JCA_IOT.h
@@ -0,0 +1,9 @@
+#ifndef _JCA_IOT_H
+#define _JCA_IOT_H
+
+#include "src/JCA_IOT_HANDLER.hpp"
+#include "src/JCA_IOT_ELEMENT.hpp"
+#include "src/JCA_IOT_ELEMENT_DI.hpp"
+#include "src/JCA_IOT_ELEMENT_DO.hpp"
+
+#endif
